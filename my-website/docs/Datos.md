56c39a21431a1416b72ce7182b9eff20e68fb654
MasterPedro1 <99909385+MasterPedro1@users.noreply.github.com>
2022-04-05 16:38:18 -0500

    Datos Personales

---
+++ my-website/docs/Datos.md
@@ -0,0 +1,5 @@
+## Nombre: Pedro Morales Cabañas
+
+## Materia: Sistemas Interpretados
+
+![Perro con peluca](https://ae01.alicdn.com/kf/Hf3af3c1902f84be7bb6841690e710df7H/Tangpan-Sombrero-ajustable-para-mascota-sombrero-corto-marr-n-para-fiesta-gato-peluca-de-perro-accesorios.jpg_Q90.jpg_.webp)
--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Pedro Morales Cabañas',
-  tagline: 'Sistemas Operativos',
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
